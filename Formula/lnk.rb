c14e78047e2c6ab61763a470ae0dfa21d737be87
Rowan Stein <rowan.stein@agyn.io>
2026-06-25 02:41:59 +0400

    Update lnk formula to v0.1.0-bvgroup.2

--- Formula/lnk.rb
+++ Formula/lnk.rb
@@ -1,26 +1,26 @@
 class Lnk < Formula
   desc "Fast LinkedIn CLI for posting, reading, and messaging"
   homepage "https://github.com/bvgroup-co/lnk"
-  version "0.1.0-bvgroup.1"
+  version "0.1.0-bvgroup.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.0-bvgroup.1/lnk_0.1.0-bvgroup.1_darwin_arm64.tar.gz"
-      sha256 "ab2ecc4db3a56e9aa1132056be6fb93b17544a4b20f86a48fdb18f333fafb26b"
+      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.0-bvgroup.2/lnk_0.1.0-bvgroup.2_darwin_arm64.tar.gz"
+      sha256 "1f34fe3912e5da44b81742644ffc8a46d4bed35f465193af048629a51eccf94a"
     else
-      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.0-bvgroup.1/lnk_0.1.0-bvgroup.1_darwin_amd64.tar.gz"
-      sha256 "51ec41f7879a64230642a7ce89f162aca7b67a570ca4a28abe90f41aebd40d10"
+      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.0-bvgroup.2/lnk_0.1.0-bvgroup.2_darwin_amd64.tar.gz"
+      sha256 "8784dce0d6100327cfe3f589426415e651bc88a17a936f66b9015497c57f22a5"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.0-bvgroup.1/lnk_0.1.0-bvgroup.1_linux_arm64.tar.gz"
-      sha256 "292b28ba4b7767b8a9b2d6e40b7f60c11c4373c4b693fa53e503860b1bb86498"
+      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.0-bvgroup.2/lnk_0.1.0-bvgroup.2_linux_arm64.tar.gz"
+      sha256 "8311aff205d1f83dc8934839edf5ee463c96efca37f30f7af54eeb7b4827f955"
     else
-      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.0-bvgroup.1/lnk_0.1.0-bvgroup.1_linux_amd64.tar.gz"
-      sha256 "456588fdf2bb7bc7c065588228ea2b3cc58fae5fc544feb56a59dbf6997bc4bd"
+      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.0-bvgroup.2/lnk_0.1.0-bvgroup.2_linux_amd64.tar.gz"
+      sha256 "6b19c7ff5df5b1ad62f1f7defeafb46082ec3684af05fdc0480ebc6155cb04b2"
     end
   end
 
