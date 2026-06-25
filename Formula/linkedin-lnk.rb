3f3962970b288e5e617149ff8a06545e4ae06514
Rowan Stein <rowan.stein@agyn.io>
2026-06-25 07:26:46 +0400

    Update linkedin-lnk to v0.1.0-bvgroup.4 (#6)
    Merge PR #6: update linkedin-lnk formula to v0.1.0-bvgroup.4.

--- Formula/linkedin-lnk.rb
+++ Formula/linkedin-lnk.rb
@@ -1,26 +1,26 @@
 class LinkedinLnk < Formula
   desc "Fast LinkedIn CLI for posting, reading, and messaging"
   homepage "https://github.com/bvgroup-co/lnk"
-  version "0.1.0-bvgroup.3"
+  version "0.1.0-bvgroup.4"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.0-bvgroup.3/lnk_0.1.0-bvgroup.3_darwin_arm64.tar.gz"
-      sha256 "04824fff6f6c67bb8fa3204cff3213cb5499216f3101e5cd6533fbf65a23a069"
+      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.0-bvgroup.4/lnk_0.1.0-bvgroup.4_darwin_arm64.tar.gz"
+      sha256 "7e552b02d035450f132f7ec7f3a6a278ebda29e37cc6350de92c568af076dc3f"
     else
-      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.0-bvgroup.3/lnk_0.1.0-bvgroup.3_darwin_amd64.tar.gz"
-      sha256 "61f601554fc97712312cde9a47818dee4b317503cd74708892ddf2a2454fac44"
+      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.0-bvgroup.4/lnk_0.1.0-bvgroup.4_darwin_amd64.tar.gz"
+      sha256 "65de2455010637a98ed2c21276dbbaa75f82ec8560c8e5bf44435802b7877073"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.0-bvgroup.3/lnk_0.1.0-bvgroup.3_linux_arm64.tar.gz"
-      sha256 "1da8cc541f2d04fbda25f908e994abf1f8f71ac0d30df87fd0f2a8fb7c9f7eb9"
+      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.0-bvgroup.4/lnk_0.1.0-bvgroup.4_linux_arm64.tar.gz"
+      sha256 "b4e2edd17db328e777c437862f998fd0f366c9ff11b5b34df9a7fd4657c314bf"
     else
-      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.0-bvgroup.3/lnk_0.1.0-bvgroup.3_linux_amd64.tar.gz"
-      sha256 "78b2849f6eda6d1f2920d6afbc0968322411134bdd49ba543e422eecd5fa86ad"
+      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.0-bvgroup.4/lnk_0.1.0-bvgroup.4_linux_amd64.tar.gz"
+      sha256 "0523fd1a1472fa462add0f4080f2f49f28dd148ee034ede2a91cb5b6aa81b0e2"
     end
   end
 
