11a54ebb325122aa22b7d12fc984f14eebb37a81
casey-brooks <casey.brooks@agyn.io>
2026-06-26 14:12:56 +0400

    fix: update linkedin-lnk formula (#17)

--- Formula/linkedin-lnk.rb
+++ Formula/linkedin-lnk.rb
@@ -1,37 +1,31 @@
 class LinkedinLnk < Formula
   desc "Fast LinkedIn CLI for posting, reading, and messaging"
   homepage "https://github.com/bvgroup-co/lnk"
-  version "0.1.0-bvgroup.11"
+  version "0.1.0-bvgroup.12"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.0-bvgroup.11/lnk_0.1.0-bvgroup.11_darwin_arm64.tar.gz"
-      sha256 "0553eaed107b7c721462ade7c7172d08f7cc68c4c5483fff5d62b11f5143bb63"
+      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.0-bvgroup.12/lnk_0.1.0-bvgroup.12_darwin_arm64.tar.gz"
+      sha256 "98bdd4d23fbb86c0068d2216cbc59e1aca7ea5ccbcd7cb466862dc687b90baeb"
     else
-      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.0-bvgroup.11/lnk_0.1.0-bvgroup.11_darwin_amd64.tar.gz"
-      sha256 "2eb938c02201acfc6e7e7aeb3a3bbe89f00d219e9cbec6c7b6b0e9245282110c"
+      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.0-bvgroup.12/lnk_0.1.0-bvgroup.12_darwin_amd64.tar.gz"
+      sha256 "7011cac9e4fc4ba88d762f32bed0e4e2f4a006b155b58d59cd3ed4985c65ce01"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.0-bvgroup.11/lnk_0.1.0-bvgroup.11_linux_arm64.tar.gz"
-      sha256 "05a130766e060d2d1e887f58a0c5f91692c3f48380181df0ffc5d7ec0db017d3"
+      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.0-bvgroup.12/lnk_0.1.0-bvgroup.12_linux_arm64.tar.gz"
+      sha256 "248c75dfebc638df34891be9531fe85e188bfbdfa8f35f29130e3003895db089"
     else
-      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.0-bvgroup.11/lnk_0.1.0-bvgroup.11_linux_amd64.tar.gz"
-      sha256 "4a3466e1114160e1d460f13bf491881510cc968ff3418d1a5bdedcc07b3e40c9"
+      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.0-bvgroup.12/lnk_0.1.0-bvgroup.12_linux_amd64.tar.gz"
+      sha256 "390ed0687ba06df87063a3ea7ff987f6352d2194d372e39969818317b484624d"
     end
   end
 
   def install
-    binary = if OS.mac?
-      Hardware::CPU.arm? ? "lnk_darwin_arm64" : "lnk_darwin_amd64"
-    else
-      Hardware::CPU.arm? ? "lnk_linux_arm64" : "lnk_linux_amd64"
-    end
-
-    bin.install binary => "lnk"
+    bin.install "lnk"
   end
 
   test do
