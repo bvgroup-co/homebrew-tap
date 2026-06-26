287bb78e6b34fbe9ba006a30e14f6d6ce7b0206c
Casey Brooks <casey.brooks@agyn.io>
2026-06-26 16:00:47 +0000

    fix: update linkedin-lnk to bvgroup.16

--- Formula/linkedin-lnk.rb
+++ Formula/linkedin-lnk.rb
@@ -1,26 +1,26 @@
 class LinkedinLnk < Formula
   desc "Fast LinkedIn CLI for posting, reading, and messaging"
   homepage "https://github.com/bvgroup-co/lnk"
-  version "0.1.0-bvgroup.15"
+  version "0.1.0-bvgroup.16"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.0-bvgroup.15/lnk_0.1.0-bvgroup.15_darwin_arm64.tar.gz"
-      sha256 "e867c5ac0ba88f49c4308292e9f951dea5e4d4c9a56a09fecccfae499d64b5f2"
+      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.0-bvgroup.16/lnk_0.1.0-bvgroup.16_darwin_arm64.tar.gz"
+      sha256 "2e6f809d2ddad9e3ea435c32a65a8d7312ea47381f656a778d05867a916a4d56"
     else
-      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.0-bvgroup.15/lnk_0.1.0-bvgroup.15_darwin_amd64.tar.gz"
-      sha256 "b2e730d6dec7455a2dfe7948b9cf7349f091b7f51117228fbb3fa7aeaff734fb"
+      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.0-bvgroup.16/lnk_0.1.0-bvgroup.16_darwin_amd64.tar.gz"
+      sha256 "921b41439c31d8704148d14a9ad1d11d8fa0571b5383af2402158390a08d811c"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.0-bvgroup.15/lnk_0.1.0-bvgroup.15_linux_arm64.tar.gz"
-      sha256 "b4e94b7e23c72d5c164c2ad95eadddd3a54fa687eb98c5c2479eea16974b6acf"
+      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.0-bvgroup.16/lnk_0.1.0-bvgroup.16_linux_arm64.tar.gz"
+      sha256 "44a956681227e4844952631608bfbeb92808091791f7f2b011acdea260d3a7fb"
     else
-      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.0-bvgroup.15/lnk_0.1.0-bvgroup.15_linux_amd64.tar.gz"
-      sha256 "330451dac8c4e7c087cc2077928fd1564fe43a50e9c4ce5941651b9617e6e8bd"
+      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.0-bvgroup.16/lnk_0.1.0-bvgroup.16_linux_amd64.tar.gz"
+      sha256 "eb838b26152d346078f92235092950201f6cec8f07448d3b5bfed269c345c83c"
     end
   end
 
