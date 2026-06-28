0f882bdf8675704e6f6bd53a1659a76cd6642872
casey-brooks <casey.brooks@agyn.io>
2026-06-28 14:20:20 +0400

    fix: update linkedin-lnk to v0.1.1 (#36)

--- Formula/linkedin-lnk.rb
+++ Formula/linkedin-lnk.rb
@@ -1,26 +1,26 @@
 class LinkedinLnk < Formula
   desc "Fast LinkedIn CLI for posting, reading, and messaging"
   homepage "https://github.com/bvgroup-co/lnk"
-  version "0.1.0-bvgroup.23"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.0-bvgroup.23/lnk_0.1.0-bvgroup.23_darwin_arm64.tar.gz"
-      sha256 "f458a9e6b476fbc261067861258329f6960dcf7070294e194d52dbc5b901ca6e"
+      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.1/lnk_0.1.1_darwin_arm64.tar.gz"
+      sha256 "b9d4dba54582ad7bb7b26cad261e66b418d337a12e93afc40df6d7fd18f701eb"
     else
-      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.0-bvgroup.23/lnk_0.1.0-bvgroup.23_darwin_amd64.tar.gz"
-      sha256 "3cad423d3178192cb475419c60c700c36863c95112f0b3a8e2f8c469354bd4fe"
+      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.1/lnk_0.1.1_darwin_amd64.tar.gz"
+      sha256 "57c4e88e926d732ddd56c075c5dbe2a1b05a74560ee950ca1f82cb9fd42558eb"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.0-bvgroup.23/lnk_0.1.0-bvgroup.23_linux_arm64.tar.gz"
-      sha256 "528de4878ab93b0c14df1cc57c7130184c0ac51402620b2e8d1ce5d3de709141"
+      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.1/lnk_0.1.1_linux_arm64.tar.gz"
+      sha256 "18217b541a9d0e80fc25da3b9f2f0a02a6f8e992c5286f05305d39163fc2d8db"
     else
-      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.0-bvgroup.23/lnk_0.1.0-bvgroup.23_linux_amd64.tar.gz"
-      sha256 "24e900a23d39e6cf492d7730281092c732df9b6bc238f289baf2c533a64ee740"
+      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.1/lnk_0.1.1_linux_amd64.tar.gz"
+      sha256 "c4a453d427a033d09fe068f4cb30d3b89f7e56be4c3ddff8920893805b094e6b"
     end
   end
 
