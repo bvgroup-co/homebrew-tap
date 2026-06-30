2204b2d54fe10aa72ee90efafccd56c4455d562f
Rowan Stein <rowan.stein@agyn.io>
2026-06-30 08:57:23 +0000

    Brew formula update for lnk version v0.1.4

--- Formula/linkedin-lnk.rb
+++ Formula/linkedin-lnk.rb
@@ -1,26 +1,26 @@
 class LinkedinLnk < Formula
   desc "Fast LinkedIn CLI for posting, reading, and messaging"
   homepage "https://github.com/bvgroup-co/lnk"
-  version "0.1.3"
+  version "0.1.4"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.3/lnk_0.1.3_darwin_arm64.tar.gz"
-      sha256 "85faa2aacb7613046fd18bed79ea293e18aab46318aa05e3c9145ab56c2b3897"
+      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.4/lnk_0.1.4_darwin_arm64.tar.gz"
+      sha256 "0220e350fb6624955de3ea252c4c49fd224aeb028f858b8f1daa3305dafd14e6"
     else
-      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.3/lnk_0.1.3_darwin_amd64.tar.gz"
-      sha256 "b51e7baf477a8b6c7f340d0141193a26a4a7e03c44befc800b07eabc6aabb83b"
+      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.4/lnk_0.1.4_darwin_amd64.tar.gz"
+      sha256 "b607ac49e31ab7dde504572b8d444bdf74e335fcb88f54ef8852938899ceac93"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.3/lnk_0.1.3_linux_arm64.tar.gz"
-      sha256 "6cca32bb4dc66eb4ab7253154d19cf6a97a40bbf8fa6151bf8210a4730e32a9a"
+      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.4/lnk_0.1.4_linux_arm64.tar.gz"
+      sha256 "ead711132af13e028f696a2691a4826cf9c16a741e77b9c31a81b58c69080f8b"
     else
-      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.3/lnk_0.1.3_linux_amd64.tar.gz"
-      sha256 "e2061e47d210a2b6591e6666f3bcfea9289952214d085b022998ad3d9fad7a9f"
+      url "https://github.com/bvgroup-co/lnk/releases/download/v0.1.4/lnk_0.1.4_linux_amd64.tar.gz"
+      sha256 "6c9b0145485c7ef02e95906779d14e28309abd0ff6a81041ddabfb860f4b4587"
     end
   end
 
